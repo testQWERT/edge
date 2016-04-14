1b0f8890bdf6139771fa85b4259be4a1b235b66b
ShawnHan1993 <shawnhan1029@gmail.com>
2016-04-14 14:43:15 +0800

    Update angleFeat.m

--- angleFeat.m
+++ angleFeat.m
@@ -1,15 +1,16 @@
 function [ result ] = angleFeat( c1,c2 )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
+divider=10;
 cosab=dot(c1,c2)/(norm(c1)*norm(c2));
 angle=rad2deg(acos(cosab));
-center=ceil(angle/20);
-result=zeros(9,1);
-for i=1:9
+center=ceil(angle/divider);
+result=zeros(round(180/divider),1);
+for i=1:round(180/divider)
         switch abs(center-i)
-            case {2,7}
+            case {2,round(180/divider)-2}
                 result(i)=0.0439;
-            case {1,8}
+            case {1,round(180/divider)-1}
                 result(i)=0.4578;
             case 0
                 result(i)=1;
